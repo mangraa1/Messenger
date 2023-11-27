da82f445271f8c4b28aa81f0418bc5ed6ad685b2
mangraa1 <georgiu.spare@gmail.com>
2023-11-27 17:47:14 +0200

    Initial Commit

---
+++ Messenger/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by mac on 27.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
