6347919f271aa62b643214a2c37c9e6c1f5883cf
mangraa1 <georgiu.spare@gmail.com>
2023-12-06 15:58:10 +0200

    Set Up Firebase
    Added Firebase to the project, setting up an application delegate

--- Messenger/Resources/MessengerApp.swift
+++ Messenger/Resources/MessengerApp.swift
@@ -9,10 +9,20 @@ import SwiftUI
 
 @main
 struct MessengerApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var appDelegate: AppDelegate
+
+
     var body: some Scene {
         WindowGroup {
             ConversationListView()
                 .environmentObject(AppStateModel())
         }
+    }
+}
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+
+        return true
     }
 }
